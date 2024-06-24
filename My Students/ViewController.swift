61a0e65b14a1d69331312511a542a7b6eaa7ec29
Mark Kulik <kulik_mark@mail.ru>
2024-06-24 08:08:38 +0300

    Initial Commit

---
+++ My Students/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My Students
+//
+//  Created by Марк Кулик on 24.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
